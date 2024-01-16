b2cb9bcc9fccf57a3d25ed9acecc5269bde3f24a
Nastya Boiko <boiko.nastasiia@gmail.com>
2024-01-16 15:19:39 +0100

    added valid layout for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './components/Main/MainPage';
 import IconsList from './components/Icons/IconsList';
 import PicturesList from './components/Pictures/PicturesList';
@@ -9,17 +9,12 @@ import ColorsList from './components/Colors/ColorsList';
 const App: React.FC = () => {
   return (
     <Router>
-      <div>
-        <Switch>
-          <Route path="/icons" render={(props) => <IconsList {...props} />} />
-          <Route
-            path="/pictures"
-            render={(props) => <PicturesList {...props} />}
-          />
-          <Route path="/colors" render={(props) => <ColorsList {...props} />} />
-          <Route path="/" render={(props) => <MainPage {...props} />} />
-        </Switch>
-      </div>
+      <Routes>
+        <Route path="/icons" element={<IconsList />} />
+        <Route path="/pictures" element={<PicturesList />} />
+        <Route path="/colors" element={<ColorsList />} />
+        <Route path="/" element={<MainPage />} />
+      </Routes>
     </Router>
   );
 };
